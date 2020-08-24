fcf0863ec724a4aa979a2c79f9a5030210683d37
fiatjaf <fiatjaf@alhur.es>
2020-08-23 22:35:44 -0300

    routing through room urls.

--- client/stores.js
+++ client/stores.js
@@ -2,34 +2,50 @@ import {readable} from 'svelte/store'
 
 import * as toast from './toast'
 
-export function getMessages(room) {
-  var list = []
+var list = []
+var roomName = null
 
-  return readable(list, set => {
-    const es = new EventSource(`/${window.btoa(room)}/receive`)
-    es.onerror = e => {
-      console.log('sse error', e.data)
-      list = []
-      set(list)
-    }
-    es.addEventListener('error', e => {
-      toast.error(e.data)
-    })
-    es.addEventListener('reset', e => {
-      console.log('reset')
-      list = []
-      set(list)
-    })
-    es.addEventListener('message', e => {
-      let message = e.data
-      list = [parseMessage(message), ...list]
-      set(list)
-    })
+var setRoom, setMessages
 
-    return () => {
-      es.close()
-    }
+export const room = readable(roomName, set => {
+  setRoom = set
+})
+
+export const messages = readable(list, set => {
+  setMessages = set
+})
+
+var es
+
+export function joinRoom(roomName) {
+  setRoom(roomName)
+
+  if (es) {
+    es.close()
+  }
+
+  es = new EventSource(`/${window.btoa(roomName)}/receive`)
+  es.onerror = e => {
+    console.log('sse error', e.data)
+    list = []
+    setMessages(list)
+  }
+  es.addEventListener('error', e => {
+    toast.error(e.data)
   })
+  es.addEventListener('reset', e => {
+    list = []
+    setMessages(list)
+  })
+  es.addEventListener('message', e => {
+    let message = e.data
+    list = [parseMessage(message), ...list]
+    setMessages(list)
+  })
+
+  return () => {
+    es.close()
+  }
 }
 
 function parseMessage(data) {
